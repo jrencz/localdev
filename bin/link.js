bb1acba7c14f9594a36c96bb605700541cda83ce
Jarek Rencz <jarek.rencz@functionite.com>
2017-02-24 10:40:10 +0100

    Release 0.1.0: No fixed hooks, rc config, dependencies, hooks and scopes
    - `rc` is used for configuring localdev from now on which means its
    configuration can be expressed both in a particular project (when
    localdev is a dependency of the project) and somewhere above
    the project’s diretory (in case is’t installed globally so that projects
    have no knowledge of its existance). rc allows ininand json syntaxes but
    json is a syntax of choice: you’re free to try using ini but all
    examples are expressed in json and features are currently developed with
    json syntax in mind.
    - projects that can be linked are no longer obliged to implement any
    npm scripts to be runnable by localdev. It’s possible to run any shell
    script as linked dependency hook. It’s expressed in `.localdevrc` what
    script is run. npm scripts are promoted syntactically.
    - scopes feature allow grouping localdev configuration.
    - if no scope is given scope is set to the path where localdev
    executable (either `link` or `start`) is called
    - configuration reference has been added to README.md
    - some eye-pleasing CLI colors were added

--- bin/link.js
+++ bin/link.js
@@ -1,92 +1,33 @@
 #!/usr/bin/env node
 
-require('json5/lib/require');
+const pkg = require('../package.json');
+const config = require('rc')(pkg.name);
 
-const npm = require('npm');
 const path = require('path');
-const fs = require('fs');
-const {isLink} = require('fs-utils');
+const hostPkg = require(path.resolve('./package.json'));
+const npm = require('npm');
+
+const forEachOfDependencies = require('../lib/forEachOfDependencies');
+const linkPackage = require('../lib/linkPackage');
+
 const {
-  magenta,
-  cyan,
-  red,
-  green,
-} = require('colors');
+  compile: compileDependencies,
+} = require('../lib/dependencies');
 
-const commonYargsConfig = require('../lib/commonYargsConfig');
-const {argv} = commonYargsConfig(require("yargs"));
-
-const pkg = require(path.resolve('./package.json'));
-const prefix = cyan('[localdev:link]');
-const log = (...msgs) => {
-  console.log(prefix, ...msgs);
-};
-const isDependency = require('../lib/isDependencyOf')(pkg);
-const config = require(path.resolve(argv.config));
-
-npm.load(pkg, (err) => {
+npm.load(hostPkg, (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
 
-  Object
-    .keys(config)
-    .forEach(locallyDevelopedDependencyName => {
-      const infix = `package ${ magenta(locallyDevelopedDependencyName) }`;
+  forEachOfDependencies({
+    compiler: compileDependencies,
+    processor: linkPackage(npm),
+    config,
+  });
 
-      if (!isDependency(locallyDevelopedDependencyName)) {
-        log(`${ infix } is NOT A DEPENDENCY. Ignoring.`);
-      }
-
-      // Note: only direct dependencies can be linked.
-      const pkgAbsPath = path.resolve('./node_modules/', locallyDevelopedDependencyName);
-      const wcRelPath = config[locallyDevelopedDependencyName];
-      const wcAbsPath = path.resolve(wcRelPath);
-
-      if (fs.existsSync(wcAbsPath)) {
-        if (isLink(pkgAbsPath)) {
-          log(`${ infix } IS ALREADY LINKED. Leaving as is.`);
-          return;
-        }
-
-        log(`${ infix } WILL BE LINKED. It was found locally under ${ cyan(wcAbsPath) }`);
-
-        npm.link(wcRelPath, (err) => {
-          if (err) {
-            log(`${ infix } LOCAL LINKING ${ red('FAILED') }.`);
-          }
-
-          log(`${ infix } LOCAL LINKING ${ green('DONE') }.`);
-        });
-      } else {
-        if (isLink(pkgAbsPath)) {
-          log(`${ infix } WILL BE UNLINKED. It no longer exists under ${ cyan(wcRelPath) }`);
-
-          npm.unlink(locallyDevelopedDependencyName, (err) => {
-            if (err) {
-              log(`${ infix } LOCAL UNLINKING ${ red('FAILED') }.`);
-              return;
-            }
-
-            log(`${ infix } LOCAL UNLINKING ${ green('DONE') }.`);
-            log(`${ infix } Will now be installed.`);
-
-            npm.install(locallyDevelopedDependencyName, (err) => {
-              if (err) {
-                log(`${ infix } INSTALL ${ red('FAILED') }.`);
-                return;
-              }
-
-              log(`${ infix } INSTALL ${ green('DONE') }.`);
-            })
-          });
-        } else if (fs.existsSync(pkgAbsPath)) {
-          log(`${ infix } IS INSTALLED. No local working copy exists.`);
-        } else {
-          log(`${ infix } is NOT there. Ignoring.`);
-        }
-      }
-    });
+  // TODO: prune (npm unlink + npm i) all those that are no longer in compiled
+  // config.
+  // To achieve this it all has to be async.
 });
 
